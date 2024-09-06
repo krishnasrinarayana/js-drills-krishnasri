1db93139fde5dfafad577905cf7cbc764c428c92
krishnasri <krishnasri.puppala.29.3@mountblue.tech>
2024-09-06 13:32:33 +0530

    cars-drill

--- test.js
+++ test.js
@@ -1,96 +1,161 @@
-let inventory = [
-    { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-    { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-    {
-      id: 3,
-      car_make: "Land Rover",
-      car_model: "Defender Ice Edition",
-      car_year: 2010,
-    },
-    { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-    { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-    { id: 6, car_make: "Audi", car_model: "riolet", car_year: 1995 },
-    { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-    { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-    { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-    { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-    { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-    { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-    { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-    { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 },
-    { id: 15, car_make: "Dodge", car_model: "Intrepid", car_year: 2000 },
-    {
-      id: 16,
-      car_make: "Mitsubishi",
-      car_model: "Montero Sport",
-      car_year: 2001,
-    },
-    { id: 17, car_make: "Buick", car_model: "Skylark", car_year: 1987 },
-    { id: 18, car_make: "Geo", car_model: "Prizm", car_year: 1995 },
-    { id: 19, car_make: "Oldsmobile", car_model: "Bravada", car_year: 1994 },
-    { id: 20, car_make: "Mazda", car_model: "Familia", car_year: 1985 },
-    { id: 21, car_make: "Chevrolet", car_model: "Express 1500", car_year: 2003 },
-    { id: 22, car_make: "Jeep", car_model: "Wrangler", car_year: 1997 },
-    { id: 23, car_make: "Eagle", car_model: "Talon", car_year: 1992 },
-    { id: 24, car_make: "Toyota", car_model: "MR2", car_year: 2003 },
-    { id: 25, car_make: "BMW", car_model: "525", car_year: 2005 },
-    { id: 26, car_make: "Cadillac", car_model: "Escalade", car_year: 2005 },
-    { id: 27, car_make: "Infiniti", car_model: "Q", car_year: 2000 },
-    { id: 28, car_make: "Suzuki", car_model: "Aerio", car_year: 2005 },
-    { id: 29, car_make: "Mercury", car_model: "Topaz", car_year: 1993 },
-    { id: 30, car_make: "BMW", car_model: "6 Series", car_year: 2010 },
-    { id: 31, car_make: "Pontiac", car_model: "GTO", car_year: 1964 },
-    { id: 32, car_make: "Dodge", car_model: "Ram Van 3500", car_year: 1999 },
-    { id: 33, car_make: "Jeep", car_model: "Wrangler", car_year: 2011 },
-    { id: 34, car_make: "Ford", car_model: "Escort", car_year: 1991 },
-    { id: 35, car_make: "Chrysler", car_model: "300M", car_year: 2000 },
-    { id: 36, car_make: "Volvo", car_model: "XC70", car_year: 2003 },
-    { id: 37, car_make: "Oldsmobile", car_model: "LSS", car_year: 1997 },
-    { id: 38, car_make: "Toyota", car_model: "Camry", car_year: 1992 },
-    { id: 39, car_make: "Ford", car_model: "Econoline E250", car_year: 1998 },
-    { id: 40, car_make: "Lotus", car_model: "Evora", car_year: 2012 },
-    { id: 41, car_make: "Ford", car_model: "Mustang", car_year: 1965 },
-    { id: 42, car_make: "GMC", car_model: "Yukon", car_year: 1996 },
-    { id: 43, car_make: "Mercedes-Benz", car_model: "R-Class", car_year: 2009 },
-    { id: 44, car_make: "Audi", car_model: "Q7", car_year: 2012 },
-    { id: 45, car_make: "Audi", car_model: "TT", car_year: 2008 },
-    { id: 46, car_make: "Oldsmobile", car_model: "Ciera", car_year: 1995 },
-    { id: 47, car_make: "Volkswagen", car_model: "Jetta", car_year: 2007 },
-    { id: 48, car_make: "Dodge", car_model: "Magnum", car_year: 2008 },
-    { id: 49, car_make: "Chrysler", car_model: "Sebring", car_year: 1996 },
-    { id: 50, car_make: "Lincoln", car_model: "Town Car", car_year: 1999 },
-  ];
+//---------------------------------------------PROBLEM-1 TEST CASE  -----------------------------------------------------------------------
+
+
+
+// Normal case
+findCarById(inventory, 33); // Expected: Car 33 is a 2011 Jeep Wrangler
+
+//No Arguments
+findCarById(); // Expected: Invalid arguments
+
+// Empty Array and Valid ID
+findCarById([], 33); // Expected: Car with id 33 not found
+
+// Inventory with Invalid Items
+findCarById([null], 33); // Expected: Car with id 33 not found
+
+// Non-existent ID
+findCarById(inventory, 999); // Expected: Car with id 999 not found
+
+//Invalid ID Type
+findCarById(inventory, "33"); // Expected: Invalid arguments
+
+
+
+
+//---------------------------------------------------------PROBLEM-2------------------------------------------------------------------
+ 
+// Normal case
+ getLastCar(inventory); // Expected: Last car is a Lincoln Town Car
+
+ // No Arguments
+ getLastCar(); // Expected: Invalid argument
+
+ // Empty Array
+ getLastCar([]); // Expected: No cars in inventory
+
+ //  Inventory with Invalid Items
+ getLastCar([null]); // Expected: Invalid car data in inventory
+
+ //Invald data
+ getLastCar([{ car_make: "Toyota" }]); // Expected: Invalid car data in inventory
+
+ // Non-Array Argument
+ getLastCar("string"); // Expected: Invalid argument
+
+
+
+
+ //------------------------------------------------------PROBLEM-3------------------------------------------------------------------------
+
+ // Normal case
+ sortCarModels(inventory); // Expected output: Sorted list of car models
+ 
+ // No Arguments
+ sortCarModels(); // Expected: Invalid argument
+ 
+ // Empty Array
+ sortCarModels([]); // Expected: No car models to sort
+ 
+ // Inventory with Invalid Items
+ sortCarModels([null]); // Expected: Invalid car data in inventory
+
+ //Invalid data
+ sortCarModels([{ car_make: "Toyota" }]); // Expected: Invalid car data in inventory
+
+ //Invalid data
+ sortCarModels([{ car_model: 12345 }]); // Expected: Invalid car data in inventory
+ 
+ // Non-Array Argument
+ sortCarModels("string"); // Expected: Invalid argument
+
+
+
+ //----------------------------------------------PROBLEM-4-----------------------------------------------------------------------------------------
+
+
+// Normal case
+  extractCarYears(inventory);// Expected output: Array of car years from the inventory
+
+  //  No Arguments
+  extractCarYears(); // Expected: Invalid argument
+  
+  //  Empty Array
+  extractCarYears([]); // Expected: No cars in inventory
+
+  //  Inventory with Invalid Items
+  extractCarYears([null]); // Expected: Invalid car data in inventory
+
+
+//invalid datat
+  extractCarYears([{ car_make: "Toyota" }]); // Expected: Invalid car data in inventory
+
+  //invalid data
+  extractCarYears([{ car_year: "2020" }]);  // Expected: Invalid car data in inventory
+  
+  // Non-Array Argument
+  extractCarYears("string"); // Expected: Invalid argument
 
 
 
 
 
-  function findCarById(inventory, id) {
-    
-    for (let i = 0; i < inventory.length; i++) {
-      if (inventory[i].id === id) {
-        console.log(`Car ${id} is a ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`);
-        return;
-      }
-    }
-    console.log(`Car with id ${id} not found`);
-  }
-  findCarById(inventory,33)
 
 
 
-  let info = [];
-const carsInfo = (inventory) => {
-  if (isValidArg(inventory)) {
-    for (let i = 0; i < inventory.length; i++) {
-      if (inventory[i].id == 33) {
-        info.push(inventory[i].car_year);
-        info.push(inventory[i].car_make);
-        info.push(inventory[i].car_model);
 
-        return inventory[i];
-      } else continue;
-    }
-    console.log(info);
-  }
-};
+
+
+
+  //-----------------------------------------------------------PROBLEM-5---------------------------------------------------------------
+
+
+// Normal case
+filterOlderCars(inventory);// Expected: Number of cars older than 2000
+
+// No Arguments
+filterOlderCars();// Expected: Invalid argument, 0
+
+//  Empty Array
+filterOlderCars([]);// Expected: No cars in inventory, 0
+
+// Inventory with Invalid Items
+filterOlderCars([null]); // Expected: Invalid car data in inventory, 0
+
+//invalid data
+filterOlderCars([{ car_make: "Toyota" }]); // Expected: Invalid car data in inventory, 0
+
+//invalid data
+filterOlderCars([{ car_year: "2020" }]); // Expected: Invalid car data in inventory, 0
+
+// Edge Case: Non-Array Argument
+filterOlderCars("string"); // Expected: Invalid argument, 0
+
+
+
+
+
+
+  //----------------------------------------------------------PROBLEM-6-------------------------------------------------------------
+
+
+  // Normal case
+  filterBMWAndAudi(inventory); // Expected: JSON string of BMW and Audi car models
+
+  //No Arguments
+  filterBMWAndAudi(); // Expected: Invalid argument, []
+
+  // Empty Array
+  filterBMWAndAudi([]);// Expected: No cars in inventory, []
+
+  // Inventory with Invalid Items
+  filterBMWAndAudi([null]);  // Expected: Invalid car data in inventory, []
+
+  //invalid data
+  filterBMWAndAudi([{ car_make: "Toyota" }]); // Expected: Invalid car data in inventory, []
+
+  //invaild data
+  filterBMWAndAudi([{ car_make: "BMW", car_model: "3 Series" }, { car_make: "Audi", car_model: "A4" }, { car_make: null }]); // Expected: Invalid car data in inventory, []
+
+  // Non-Array Argument
+  filterBMWAndAudi("string"); // Expected: Invalid argument, []
